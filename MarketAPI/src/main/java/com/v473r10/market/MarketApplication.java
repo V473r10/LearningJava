eda377f1a876d3ae49156ea7dfcd572c37b6768b
V473R10 <fac.valerio@gmail.com>
2023-01-26 01:32:18 -0300

    Initial Commit

---
+++ MarketAPI/src/main/java/com/v473r10/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package com.v473r10.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+	}
+
+}
---
+++ MarketAPI/src/test/java/com/v473r10/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v473r10.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
